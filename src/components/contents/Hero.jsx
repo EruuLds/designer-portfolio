af28a7fae4bdb6d7be4c60ff0169ed5cc0120bec
Hugo Erhubey Quintana Ledesma <eruledesma@MacBook-Air-de-Hugo.local>
2026-04-13 09:55:12 -0600

    meeeeeeeeh

--- src/components/contents/Hero.jsx
+++ src/components/contents/Hero.jsx
@@ -9,13 +9,13 @@ export default function Hero() {
     return (
         <header className="relative h-svh w-full flex flex-col justify-center items-center">
             <div className="absolute w-full h-full z-[0]">
-                <Silk />
+                <Silk speed={0}/>
             </div>
             <div className="responsive-container text-center z-[10]">
                 <h1 className="text-6xl">Erhubey Ledesma</h1>
                 <GradientText
                     colors={["#FFC933", "#FFEA73", "#FFC933"]}
-                    
+                    animationSpeed={0}
                     className="mb-8"
                 >
                     <p className="text-4xl font-semibold text-balance">
